0186f52b537444d8370b98f625d92f45bcb69f64
VOGEL Thierry <thierry.vogel@orange.com>
2016-01-03 11:25:18 +0100

    exercice5 Q

---
+++ 5_ex.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define PI 3.14159
+void exerciceA() {
+  int r;
+  double p, s;
+  printf("donnez le rayon entier d'un cercle : ");
+  scanf("%d",&r);
+  p=2*PI*r;
+  s=PI*r*r;
+  printf("le cercle de rayon %d ", r);
+  printf("a un perimetre de %f et une surface de %f\n",p,s);
+}
